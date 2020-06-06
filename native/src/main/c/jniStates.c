f52879b8e05b3ffc9189a49beec73e1199357b99
slab <slab@c240g5-110219.wisc.cloudlab.us>
2020-06-05 21:45:11 -0500

    initial transition of state functions in JNI to hypercalls

--- native/src/main/c/jniStates.c
+++ native/src/main/c/jniStates.c
@@ -3,36 +3,50 @@
 #include <jni.h>
 #include <stdbool.h>
 
-int stateDB[100]={0};
-int maxStateDB[100]={0};
-bool set=false;
+#include <stdint.h>
+#include <string.h> 
+
+#include "include/uhcall.h"
+#include "include/uhstateDB.h"
+
+__attribute__((aligned(4096))) __attribute__((section(".data"))) uhstatedb_param_t uhcp;
+
+void init(void *bufptr) {
+  uhstatedb_param_t *ptr_uhcp = (uhstatedb_param_t *)bufptr;
+  uhcall(UAPP_UHSTATEDB_FUNCTION_INIT, ptr_uhcp, sizeof(uhstatedb_param_t));
+}
 
 JNIEXPORT void JNICALL Java_org_opendaylight_l2switch_NativeStuff_initState(JNIEnv *env, jobject obj, jintArray maxArray, jint numStates) {
-  if (!set){
-    jint *c_array;
-    c_array = (*env)->GetIntArrayElements(env, maxArray, NULL);
-    int i;
-    for(i=0; i<numStates; i++) {
-      maxStateDB[i]=c_array[i];
-    }
-    (*env)->ReleaseIntArrayElements(env, maxArray, c_array, 0);
-    set=true;
-  }
+  jint *c_array;
+  c_array = (*env)->GetIntArrayElements(env, maxArray, NULL);
+  memcpy(&uhcp.maxArray, c_array, numStates);
+  uhcp.vaddr = (uint32_t)&uhcp;
+  init((void *)&uhcp);
+  (*env)->ReleaseIntArrayElements(env, maxArray, c_array, 0);
+}
+
+void get(void *bufptr) {
+  uhstatedb_param_t *ptr_uhcp = (uhstatedb_param_t *)bufptr;
+  uhcall(UAPP_UHSTATEDB_FUNCTION_GET, ptr_uhcp, sizeof(uhstatedb_param_t));
 }
 
 JNIEXPORT jint JNICALL Java_org_opendaylight_l2switch_NativeStuff_getState(JNIEnv *env, jobject obj, jint IDnum) {
-  return stateDB[IDnum];
-}
-
-bool canTransition(int IDnum){
-  bool out=false;
-  if(stateDB[IDnum]<maxStateDB[IDnum])
-    out=true;
+  jint out;
+  uhcp.deviceID = IDnum;
+  uhcp.vaddr = (uint32_t)&uhcp;
+  get((void *)&uhcp);
+  out = uhcp.stateVal;
   return out;
 }
 
+void next(void *bufptr) {
+  uhstatedb_param_t *ptr_uhcp = (uhstatedb_param_t *)bufptr;
+  uhcall(UAPP_UHSTATEDB_FUNCTION_NEXT, ptr_uhcp, sizeof(uhstatedb_param_t));
+}
+
 JNIEXPORT void JNICALL Java_org_opendaylight_l2switch_NativeStuff_transitionState(JNIEnv *env, jobject obj, jint IDnum) {
-  if(canTransition(IDnum)) 
-    stateDB[IDnum]++;
+  uhcp.deviceID = IDnum;
+  uhcp.vaddr = (uint32_t)&uhcp;
+  next((void *)&uhcp);
 }
   
