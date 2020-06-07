5becfd8da2917be63431c5ecbc49085c8b90af00
slab <you@example.com>
2020-06-06 20:44:08 -0400

    updating rpi scripts

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@ setupJava(){
     export PATH=$PATH:$JAVA_HOME/bin/
     export M2_HOME=/usr/share/maven/
     export M2=$M2_HOME
-    export MAVEN_OPTS='-Xmx512m -XX:MaxPermSize=512m -Xms256m'
+    export MAVEN_OPTS='-Xmx640m -Xms256m'
     export PATH=$M2:$PATH
 }
 
@@ -19,4 +19,4 @@ fi
 
 setupJava
 
-mvn clean install -Pq -DskipTests -Dcheckstyle.skip=true -Dmaven.javadoc.skip=true
+mvn clean install -s /home/pi/.m2/settings.xml -Pq -DskipTests -Dcheckstyle.skip=true -Dmaven.javadoc.skip=true
--- startODL.sh
+++ startODL.sh
@@ -17,23 +17,14 @@ if [[ $DIR == *"l2switch"* ]]; then
     DIR=$(echo $DIR | cut -d '/' -f -3)
 fi
 
+
 setupJava
 
-# for some reason, maven is not downloading the jar for org.apache.karaf.jaas.boot:4.1.7, so we can manually download this.
-addFiles(){
-    FILE="$DIR/l2switch/distribution/karaf/target/assembly/system/org/apache/karaf/jaas/org.apache.karaf.jaas.boot/4.1.7/org.apache.karaf.jaas.boot-4.1.7.jar"
-    # first check if we have already installed it.
-    if [[ ! -f "$FILE" ]]; then
-	mkdir -p $DIR/l2switch/distribution/karaf/target/assembly/system/org/apache/karaf/jaas/org.apache.karaf.jaas.boot/4.1.7/
-	touch $FILE
-	wget -q -O - https://repo1.maven.org/maven2/org/apache/karaf/jaas/org.apache.karaf.jaas.boot/4.1.7/org.apache.karaf.jaas.boot-4.1.7.jar > $DIR/l2switch/distribution/karaf/target/assembly/system/org/apache/karaf/jaas/org.apache.karaf.jaas.boot/4.1.7/org.apache.karaf.jaas.boot-4.1.7.jar
-    fi
-}
 
-export JAVA_MAX_MEM=512M
+export JAVA_MAX_MEM=640M
 export JAVA_MAX_PERM_MEM=448M
 
 # start ODL
 cd $DIR/l2switch/distribution/karaf/target/assembly
 #sudo -E ./bin/karaf
-./bin/karaf 
+sudo -E ./bin/karaf clean
