0f40fef93a7f5bfdedd31e556d7f99582d5adff6
slab <slab@dataplane.radio.psi-pg0.wisc.cloudlab.us>
2020-10-23 18:50:33 -0500

    demo of building radio protections after analyzing traffic

--- l2switch-main/impl/src/main/java/org/opendaylight/l2switch/flow/middleboxmgmnt/MsgAnalysis.java
+++ l2switch-main/impl/src/main/java/org/opendaylight/l2switch/flow/middleboxmgmnt/MsgAnalysis.java
@@ -28,9 +28,8 @@ import java.io.FileInputStream;
 import java.io.FileWriter;
 import java.io.IOException;
 import org.opendaylight.l2switch.flow.docker.utils.ExecShellCmd;
-
+import org.opendaylight.l2switch.flow.middlebox.BuildTar;
 import java.sql.Timestamp;
-
 import java.util.Iterator; 
 import java.util.List;
 import java.util.ArrayList;
@@ -237,6 +236,9 @@ public class MsgAnalysis {
 	if (feature.getMbox().equals("radio")) {
 	    trigger = processRadioMsg(feature.getKey());
 	}
+	if (feature.getMbox().equals("learn_radio")) {
+	    trigger = processLearnRadioMsg(feature.getKey());
+	}	
 	if (feature.getMbox().equals("dos")){
 	    trigger = processDosMsg(feature.getKey());
 	}
@@ -266,6 +268,47 @@ public class MsgAnalysis {
     	return out;
     }
 
+    private boolean processLearnRadioMsg(String transitionkey){
+	boolean out = false;
+	String[] radioMsgParts = msg.split(",");
+	String modelData = radioMsgParts[0].replace(';', ',');
+	String protoData = radioMsgParts[1];
+	String portData = radioMsgParts[2];
+	if(!(modelData.substring(0,6).equals("states") && protoData.substring(0,5).equals("#type")))
+	    return out;
+   	int curStateIndex = policyMap.get(srcMac).getStateNum();	
+	File model = new File(policy.getProtections()[curStateIndex].getAddFiles()[0]);
+	File proto = new File(policy.getProtections()[curStateIndex].getAddFiles()[1]);
+	File port = new File(policy.getProtections()[curStateIndex].getAddFiles()[2]);	
+	try{
+	    FileWriter writeModel = new FileWriter(model, false);
+	    writeModel.write(modelData);
+	    writeModel.close();
+	    FileWriter writeProto = new FileWriter(proto, false);
+	    writeProto.write(protoData);
+	    writeProto.close();
+	    FileWriter writePort = new FileWriter(port, false);
+	    writePort.write(portData);
+	    writePort.close();
+	}catch(IOException e){
+	    System.out.println("IOException: "+e.getMessage());
+	}
+	ExecShellCmd obj = new ExecShellCmd();
+	String radio_rules_loc = "/etc/sec_gate/radio/local.rules";
+
+	String cmd = String.format("/usr/bin/python3 /etc/sec_gate/radio/newModel2Rule.py -M %s -P %s -s %s -n 'RADIO' -R %s", policy.getProtections()[curStateIndex].getAddFiles()[0], policy.getProtections()[curStateIndex].getAddFiles()[1], policy.getProtections()[curStateIndex].getAddFiles()[2], radio_rules_loc);
+	model.delete();
+	proto.delete();
+	port.delete();
+	String output = obj.exeCmd(cmd);
+	BuildTar tarTool = new BuildTar();
+    	int nextStateIndex = policyMap.get(srcMac).getStateNum() + 1;
+    	String tarpath = policy.getProtections()[nextStateIndex].getImageOpts()[0].getArchives()[0].getTar();	    
+	tarTool.file2Tar(tarpath, radio_rules_loc);
+	out=true;
+	return out;
+    }
+    
     
     private boolean processSnortMsg(String transitionKey) {
 	boolean out = false;
