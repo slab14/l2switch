ec048e65b3a80b4bfd3e11c9fef5877163969bfe
slab <slab@dataplane.radio.psi-pg0.wisc.cloudlab.us>
2020-10-23 12:55:23 -0500

    version working with mud policies

--- l2switch-main/impl/src/main/java/org/opendaylight/l2switch/flow/middleboxmgmnt/BuildTar.java
+++ l2switch-main/impl/src/main/java/org/opendaylight/l2switch/flow/middleboxmgmnt/BuildTar.java
@@ -8,37 +8,96 @@
 
 package org.opendaylight.l2switch.flow.middlebox;
 
-import org.apache.commons.compress.utils.IOUtils;
-import org.apache.commons.compress.archivers.ArchiveOutputStream;
-import org.apache.commons.compress.archivers.ArchiveStreamFactory;
-import org.apache.commons.compress.archivers.tar.TarArchiveEntry;
-import org.apache.commons.compress.archivers.ArchiveException;
-import org.rauschig.jarchivelib.*;
 import java.io.File;
-import java.io.OutputStream;
-import java.io.FileOutputStream;
 import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.util.Iterator; 
-import java.util.List;
+import java.io.OutputStream;
+import java.lang.Thread;
+import java.sql.Timestamp;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
-import java.lang.Thread;
+import java.util.Iterator;
+import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+import org.apache.commons.compress.archivers.ArchiveException;
+import org.apache.commons.compress.archivers.ArchiveOutputStream;
+import org.apache.commons.compress.archivers.ArchiveStreamFactory;
+import org.apache.commons.compress.archivers.tar.TarArchiveEntry;
+import org.apache.commons.compress.utils.IOUtils;
+import org.rauschig.jarchivelib.*;
+
+
 
 public class BuildTar{
     public void file2Tar(String tarpath, String infile) {
 	File f = new File(tarpath);
 	File dest = new File(f.getParent());
 	File data = new File(infile);
+    	if(f.exists()){
+	    Timestamp ts = new Timestamp(System.currentTimeMillis());
+	    File old = new File(tarpath+"."+ts.getTime());
+	    f.renameTo(old);
+	}	
 	try{
 	    Archiver archiver = ArchiverFactory.createArchiver("tar");
 	    archiver.create(f.getName(), dest, data);	    		
 	}catch(IOException e){
 	    System.out.println("IOException: " + e.getMessage());
+	}
+    }
+
+    public void ports2Tar(String tarpath, ArrayList<String> ports, String action){
+	File f = new File(tarpath); 
+    	File dest = new File(f.getParent()); 
+    	File rules = new File(f.getParent() + "/setup_iptables.sh");
+	String msg="";
+    	if(f.exists()){
+	    Timestamp ts = new Timestamp(System.currentTimeMillis());
+	    File old = new File(tarpath+"."+ts.getTime());
+	    f.renameTo(old);
+	}
+    	try{
+	    FileWriter writer = new FileWriter(rules, false);
+	    if(action.equals("allow")){
+		// set default drop rules
+		msg = "iptables -P INPUT DROP\n";
+		writer.write(msg);
+		msg = "iptables -P FORWARD DROP\n";
+		writer.write(msg);
+		msg = "iptables -P OUTPUT DROP\n";
+		writer.write(msg);		
+		for(String port:ports){
+		    msg = String.format("iptables -A FORWARD -p tcp --dport %s -j ACCEPT\n", port);
+		    writer.write(msg);
+		    msg = String.format("iptables -A FORWARD -p tcp --sport %s -j ACCEPT\n", port);
+		    writer.write(msg);				    
+		    msg = String.format("iptables -A INPUT -p tcp --dport %s -j ACCEPT\n", port);
+		    writer.write(msg);				    
+		    msg = String.format("iptables -A INPUT -p tcp --sport %s -j ACCEPT\n", port);
+		    writer.write(msg);
+		}
+	    } else if (action.equals("drop")){
+		for(String port:ports){
+		    msg = String.format("iptables -A FORWARD -p tcp --dport %s -j DROP\n", port);
+		    writer.write(msg);
+		    msg = String.format("iptables -A FORWARD -p tcp --sport %s -j DROP\n", port);
+		    writer.write(msg);
+		    msg = String.format("iptables -A INPUT -p tcp --dport %s -j DROP\n", port);
+		    writer.write(msg);
+		    msg = String.format("iptables -A INPUT -p tcp --sport %s -j DROP\n", port);
+		    writer.write(msg);		    
+		}
+	    }
+	    writer.close();
+	    Archiver archiver = ArchiverFactory.createArchiver("tar");
+	    archiver.create(f.getName(), dest, rules);	    		
+	    rules.delete(); 
+	}catch(IOException ioe){
+	    System.out.println("IOException: " + ioe.getMessage());
 	}
     }
 
